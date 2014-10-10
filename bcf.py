2aa4aa028c1cd6c7d7383492bb5aaa38807d6362
Daniel Cook <danielecook@gmail.com>
2014-10-10 00:23:12 -0500

    Added qual and filter to variant dicts for report generation.

--- bcf.py
+++ bcf.py
@@ -1,97 +1,9 @@
+#! /user/bin/env python
 # Small wrapper for bcftools 2.0 - for querying vcf data quickly and easily.
 import os, subprocess, re, glob, hashlib, gzip, mimetypes, tempfile
 from itertools import groupby as g
 from collections import OrderedDict
-
-
-def md5(filename):
-    """Performs an md5 digest on a given file"""
-    try:
-        subprocess.check_output("which md5sum", shell=True) # Checks to see if md5sum exists.
-        md5_command = "md5sum"
-    except:
-        md5_command = "md5"
-    md5_result = subprocess.Popen('%s %s' % (md5_command, filename), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0].replace("\n","")
-    return md5_result.split("=")[1].strip()
- 
-
-def most_common(L):
-    return max(g(sorted(L)), key=lambda(x, v):(len(list(v)),-L.index(x)))[0]
- 
-class fastq(object):
-    def __init__(self, fq1_filename, fq2_filename = None):
-        self.fq1_filename = fq1_filename
-        self.fq1 = self.extract_fastq_info(fq1_filename)
-        self.fq_set = [self.fq1_filename]
-        if fq2_filename is not None:
-            self.fq2_filename = fq2_filename
-            self.fq2 = self.extract_fastq_info(fq2_filename)
-            self.fq_set += [self.fq2_filename]
-        
-    def extract_fastq_info(self, fq):
-        """
-        This function will extract information from the header lines of a demultiplexed fastq. Requires gzip to be imported.
-        """
-        line = {}
-        if mimetypes.guess_type(fq)[1] == 'gzip':
-            line["filetype"] = "gzip"
-            f = gzip.open(fq, 'rb')
-        else:
-            line["filetype"] = "text"
-            f = open(fq, 'rb')
-        header_lines = [x.replace("\n","") for k,x in enumerate(f.readlines(10000)) if k%4==0]
-        c = 0
-        for heading in header_lines:
-            c += 1
-            l = re.split(r'(\:|#| )',heading)
-            index_set = []
-            if len(l) == 11:
-                line["instrument"] = l[0]
-                line["flowcell_lane"] = int(l[2])
-                try:
-                    line["pair"] = l[10].split("/")[1]
-                    index_set.append(l[10].split("/")[0])
-                except:
-                    pass
-            elif len(l) == 21:
-                line["instrument"] = l[0]
-                line["run_id"] = l[2]
-                line["flowcell_id"] = l[4]
-                line["flowcell_lane"] = l[6]
-                line["pair"] = int(l[14])
-                line["filtered"] = l[16]
-                line["control_bits"] = l[16]
-                line["index"] = l[20]
-                index_set.append(l[20])
-            else:
-                print "error", l
-            line["index"] = most_common(index_set)
-        line["fastq_filename"] = fq
-        line["md5"] = md5(fq)
-        return line
- 
-    def get_fastq_stats(self, fq_name):
-        """
-            This function will extract addition information from a fastq: Number of reads, unique reads, etc.
-        """
-        fq = getattr(self, fq_name)
-        if fq["filetype"] == "gzip":
-            command = "gunzip -c %s | awk -v filename=%s '((NR-2)%%4==0){read=$1; total++; count[read]++}END{for(read in count){if(!max||count[read]>max) {max=count[read];maxRead=read};if(count[read]==1){unique++}};print filename,total,unique,unique*100/total,maxRead,count[maxRead],count[maxRead]*100/total}'" % (fq["fastq_filename"], fq["fastq_filename"])
-        else:
-            command = "awk -v filename=%s '((NR-2)%%4==0){read=$1; total++; count[read]++}END{for(read in count){if(!max||count[read]>max) {max=count[read];maxRead=read};if(count[read]==1){unique++}};print filename,total,unique,unique*100/total,maxRead,count[maxRead],count[maxRead]*100/total}' %s" % (fq["fastq_filename"], fq["fastq_filename"])
-        # Get stat results
-        sr = subprocess.check_output(command, shell=True).strip().split(" ")
-        fq["Number of Reads"] = sr[1]
-        fq["Unique Reads"] = int(sr[2])
-        fq["Frequency of Unique Reads"] = float(sr[3])
-        fq["Most Abundant Sequence"] = sr[4]
-        fq["Number of Times Most Abundant Sequence Occcurs"] = int(sr[5])
-        fq["Frequency of Most Abundant Sequence"] = sr[6]
-
-    def fastq_stats(self):
-        self.get_fastq_stats("fq1")
-        if len(self.fq_set) == 2:
-            self.get_fastq_stats("fq2")
+from utils import *
 
 def format_options(options):
     formatted_options = {}
@@ -112,7 +24,7 @@ def format_options(options):
             formatted_options[k] = "'%s'" % v
     return formatted_options
 
-class bcf(file):
+class vcf(file):
     def __init__(self, filename):
         # Start by storing basic information about the vcf/bcf and checking that an index exists.
         self.filename = filename
@@ -174,8 +86,10 @@ class bcf(file):
             variant["ID"] = line[2]
             variant["REF"] = line[3]
             variant["ALT"] = line[4]
+            variant["QUAL"] = float(line[5])
+            variant["FILTER"] = line[6]
             variant_set.append(variant)
-            print variant
+        return variant_set
 
         p.communicate() 
         
@@ -260,8 +174,8 @@ class bcf(file):
             subprocess.check_output("bcftools index %s" % out_filename, shell=True)
 
 
-x = bcf("NIC276.nofilter.group.bcf")
-x.filter({"include":'%QUAL>30', "soft-filter":"MaxQualityFail"})
-x.filter({"include":'DP>3', "s": "MinimumDepth"})
-x.out("NIC276.vcf.gz", version = 4.2)
+#x = bcf("NIC276.nofilter.group.bcf")
+#x.filter({"include":'%QUAL>30', "soft-filter":"MaxQualityFail"})
+#x.filter({"include":'DP>3', "s": "MinimumDepth"})
+#x.out("NIC276.vcf.gz", version = 4.2)
 
