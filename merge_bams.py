997d45ea9fbfc876494604186e9ae17dd0699b92
Daniel Cook <danielecook@gmail.com>
2014-12-29 00:49:15 -0600

    Fixed issue with tempfile deletion.

--- merge_bams.py
+++ merge_bams.py
@@ -32,8 +32,8 @@ bam_dir = "{OPTIONS.analysis_dir}/{OPTIONS.bam_dir}".format(**locals())
 if len(SM_Bams) > 1:
     merge_options = format_command(align["merge"])
     merged_bam_name = SM + ".bam"
-    SM_Bams = " ".join([bam_dir + "/" + x for x in SM_Bams])
-    merge_bams = """samtools merge -f {merge_options} {bam_dir}/{merged_bam_name} {SM_Bams}""".format(**locals())
+    SM_Bam_set = " ".join([bam_dir + "/" + x for x in SM_Bams])
+    merge_bams = """samtools merge -f {merge_options} {bam_dir}/{merged_bam_name} {SM_Bam_set}""".format(**locals())
     command(merge_bams, c_log)
 else:
     # Move single_sequence bam to merged name.
@@ -52,6 +52,7 @@ command("samtools index {bam_dir}/{SM}.bam".format(**locals()), c_log)
 #========================#
 
 if COMMANDS.align.alignment_options.remove_temp == True:
+    print SM_Bams
     for bam in SM_Bams:
         command("rm {bam_dir}/{bam}".format(**locals()), c_log)
         dup_report = bam.replace(".bam",".duplicate_report.txt")
